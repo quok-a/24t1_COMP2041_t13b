edf09daf38db63ec4797570342a56e610920407e
Hanan Hassan <h.hassan@student.unsw.edu.au>
2024-02-29 22:13:01 +1100

    tute03 working

--- tute03/grades.sh
+++ tute03/grades.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-while read id mark
+while read id mark _;
 do
     # ... insert mark/grade checking here ...
+    # ps >= 50 && <65
+    # fl < 50
+    # cr >= 65 && <75
+    # dn >= 75 && <85
+    # hd >= 85 && <= 100
+    # err
+
+    # check for valid grade
+    # negative, or greater than 100 or not a numebr
+    if test "$mark" -eq "$mark" 2> /dev/null && test "$mark" -ge 0 && test "$mark" -le 100;
+    then
+        if test "$mark" -lt 50; then 
+            echo FL
+        # etc
+        fi
+    else
+        echo "?? ($mark)" >& 2
 done
--- tute03/seq.sh
+++ tute03/seq.sh
@@ -1,1 +1,43 @@
 #! /usr/bin/env dash
+
+if test $# -eq 1; then
+    start=1
+    increment=1
+    last=$1
+elif test $# -eq 2; then
+    start=$1
+    increment=1
+    last=$2
+elif test $# -eq 3; then
+    start=$1
+    increment=$2
+    last=$3
+else
+    echo "usage: $0 [FIRST [INCREMENT]] LAST" >& 2
+    exit 1
+fi
+
+# args are integers
+if test $start -eq $start 2> /dev/null; then
+else 
+    echo "$0 error: start must be an integer" >& 2
+    exit 1
+fi
+# increment is positive
+if test $increment -gt 0 2> /dev/null; then
+else 
+    echo "$0 error: increment must be greater than 0" >& 2
+    exit 1
+fi
+# last > first
+if test $last -gt $first 2> /dev/null; then
+else 
+    echo "$0 error: last must be greater than first" >& 2
+    exit 1
+fi
+
+cur=$start
+while test $cur -le $last; do
+    echo $cur
+    cur=$((cur + increment))
+done
